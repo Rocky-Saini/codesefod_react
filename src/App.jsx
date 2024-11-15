717e0b9dadfb048a85a880068dba172e064d18ba
Rocky Saini <robinr@neuronimbus.com>
2024-11-15 11:56:45 +0530

    Changes Updated

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div>
+      <h1>Welcome to CodeSefod !</h1>
+      <p>Website Coming soon...</p>
+     </div>
+     
+    </>
+  )
+}
+
+export default App
